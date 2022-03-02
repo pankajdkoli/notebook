a02c29954b4658fb55fa2a53c8e74c0e86b53bc7
pankajdkoli <pankajdkoli@gmail.com>
2022-03-02 15:03:32 +0530

    these are schema file's

---
+++ models/Notes.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+
+
+const NotesSchema = new Schema({
+
+    title: {
+        type: String,
+        require: true
+    },
+
+    description: {
+        type: String,
+        require: true,
+    },
+    tags: {
+        type: String,
+        default : "General"
+    },
+    date: {
+        type: Date,
+        require: Date.now
+    },
+
+});
+
+module.export  = mongoose.model('notes', NotesSchema); 
